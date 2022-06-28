6310b25f19a4494e44b81760b79162b3b00c5fb2
latypovroman <jiggawhat5@gmail.com>
2022-06-29 00:31:01 +0500

    added profile layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from "../Footer/Footer";
 import React from "react";
 import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
 
 
 function App() {
@@ -14,10 +15,12 @@ function App() {
               <Header/>
           <Routes>
               <Route exact path="/" element={ <Main/> }/>
-              <Route path="/movies" element={ <Movies/> }/>
-              <Route path="/saved-movies" element={ <SavedMovies/> }/>
+              <Route path="/movies" element={ <><Movies/><Footer/></> }/>
+              <Route path="/saved-movies" element={<><SavedMovies/><Footer/></>}/>
+              <Route path="/profile" element={ <Profile/> }/>
+              {/*<Route path="/movies" element={ <Footer />}/>*/}
           </Routes>
-          <Footer/>
+          {/*<Footer/>*/}
       </div>
   );
 }
